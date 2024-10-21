5e83a48bd46bce639e2b8298fb5e9940da19119d
Johandry Mora <johandrydev@gmail.com>
2024-10-20 23:28:01 -0500

    Add env file

--- cmd/accounts/main.go
+++ cmd/accounts/main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+
 	"BlueSoftBank/internal/database"
 	"BlueSoftBank/internal/handlers"
 	httpbs "BlueSoftBank/internal/pkg/http_bs"
-	"net/http"
 )
 
 func main() {
+	// import the .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v\n", err)
+	}
 	// create a new mux router
 	mux := http.NewServeMux()
 
@@ -21,7 +31,9 @@ func main() {
 	)
 
 	// start the server
-	if err := http.ListenAndServe(":8080", mux); err != nil {
-		panic(err)
+	port := os.Getenv("ACCOUNT_SERVICE_PORT")
+	log.Println("Starting server on port", port)
+	if err := http.ListenAndServe(":"+port, mux); err != nil {
+		log.Fatalf("Error starting server: %v\n", err)
 	}
 }
